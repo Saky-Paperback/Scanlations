e8fe05be3e35d7c0d6edce0d2fc2c8df8958f8ed
Saky-Paperback <69586589+Saky-Paperback@users.noreply.github.com>
2020-08-12 17:09:25 +0100

    reaper

--- dist/models/Manga/Manga.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MangaStatus = void 0;
-var MangaStatus;
-(function (MangaStatus) {
-    MangaStatus[MangaStatus["ONGOING"] = 1] = "ONGOING";
-    MangaStatus[MangaStatus["COMPLETED"] = 0] = "COMPLETED";
-})(MangaStatus = exports.MangaStatus || (exports.MangaStatus = {}));
